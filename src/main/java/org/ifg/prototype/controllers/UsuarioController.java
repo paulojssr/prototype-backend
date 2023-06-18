71998edae3dec84358d294ea6d8b0c49f8a76982
Daniel Lima Leite <lldaniellimall@gmail.com>
2023-06-17 21:49:39 -0300

    Cadastro de usuarios

--- src/main/java/org/ifg/prototype/controllers/UsuarioController.java
+++ src/main/java/org/ifg/prototype/controllers/UsuarioController.java
@@ -1,25 +1,85 @@
 package org.ifg.prototype.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.ifg.prototype.dto.UsuarioDTO;
+import org.ifg.prototype.entities.Usuario;
 import org.ifg.prototype.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService service;
 
 	@GetMapping
-	public ResponseEntity<List<UsuarioDTO>> findAll(){
+	public ResponseEntity<List<UsuarioDTO>> findAll() {
 		List<UsuarioDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping("/buscar/{codigo}")
+	public ResponseEntity<Object> buscarUsuario(@PathVariable(value = "codigo") Long codigo) {
+		Optional<UsuarioDTO> usuarioDTOOptional = service.findById(codigo);
+
+		if (!usuarioDTOOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+		} else {
+			return ResponseEntity.status(HttpStatus.OK).body(usuarioDTOOptional.get());
+		}
+	}
+
+	@PatchMapping("/atualizarusuario/{codigo}")
+	public ResponseEntity<Object> usuarioUpdate(@PathVariable(value = "codigo") Long codigo,
+			@RequestBody UsuarioDTO usuarioDTO) {
+
+		usuarioDTO.setCodigo(codigo);
+
+		Optional<UsuarioDTO> usuarioDTOOptional = service.findById(codigo);
+
+		if (usuarioDTOOptional.isPresent()) {
+
+			Usuario usuario = new Usuario();
+
+			usuario.setCodigo(usuarioDTOOptional.get().getCodigo());
+			usuario.setNome(usuarioDTOOptional.get().getNome());
+			usuario.setEmail(usuarioDTOOptional.get().getEmail());
+			usuario.setSenha(usuarioDTOOptional.get().getSenha());
+
+			service.update(usuario);
+
+			return ResponseEntity.status(HttpStatus.OK).body("Usuário atualizado");
+
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+		}
+
+	}
+
+	@PostMapping(value = "/salvar")
+	public ResponseEntity<Object> salvarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+		try {
+
+			Usuario usuario = new Usuario();
+
+			usuario.setCodigo(service.save(usuario).getCodigo());
+
+			return ResponseEntity.ok().body(usuario);
+
+		} catch (Exception ex) {
+			return ResponseEntity.badRequest().body(ex.getMessage());
+		}
 	}
 }
--- src/main/java/org/ifg/prototype/services/UsuarioService.java
+++ src/main/java/org/ifg/prototype/services/UsuarioService.java
@@ -4,6 +4,7 @@
 package org.ifg.prototype.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.ifg.prototype.dto.UsuarioDTO;
@@ -13,20 +14,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * @author paulojorge
- *
- */
 @Service
 public class UsuarioService {
 
 	@Autowired
-	private UsuarioRepository repository;
-	
+	private final UsuarioRepository usuarioRepository;
+
+	public UsuarioService(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
 	@Transactional(readOnly = true)
-	public List<UsuarioDTO> findAll(){
-		List<Usuario> result = repository.findAll();
-		return result.stream().map(x -> new UsuarioDTO(x))
-				.collect(Collectors.toList());
+	public List<UsuarioDTO> findAll() {
+		List<Usuario> result = usuarioRepository.findAll();
+
+		return result.stream().map(UsuarioDTO::new).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public Optional<UsuarioDTO> findById(Long id) {
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+		return usuarioOptional.map(UsuarioDTO::new);
+	}
+
+	@Transactional
+	public Usuario save(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+
+	@Transactional
+	public void update(Usuario usuario) {
+		usuarioRepository.save(usuario);
+	}
+
+	public void deleteUsuario(Long id) {
+		usuarioRepository.deleteById(id);
+	}
+
+	public void exist(Long id) {
+		usuarioRepository.existsById(id);
 	}
 }
